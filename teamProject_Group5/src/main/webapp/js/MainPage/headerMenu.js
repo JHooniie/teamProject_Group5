be1770f4bebacca5cdcf29e371c3876d3f7aaad7
JHooniie <102460116+JHooniie@users.noreply.github.com>
2022-06-27 09:52:25 +0900

    Revert "add loginPage"

--- teamProject_Group5/src/main/webapp/js/MainPage/headerMenu.js
+++ teamProject_Group5/src/main/webapp/js/MainPage/headerMenu.js
@@ -14,71 +14,9 @@ $(document).ready(function() {
 		function(){
 					$('.top-ani').animate({left:0}, 3000);
 				});*/
-
-	//2개의 이벤트 처리 : 방법1
-/* 					$('#btn')
-						.on('mouseover', function(){
-							$(this).css('background-color', "yellow");
-						})
-						.on('mouseout', function(){
-							$(this).css('background-color', "lime");
-						}); 
-					
-					//2개의 이벤트 처리 : 방법2
-					$('#btn').on({
-						mouseover:function(){
-							$(this).css('background-color', "red");
-						},
-						mouseout:function(){
-							$(this).css('background-color', "pink");
-						}
-					}); */
-
-					/*$('#top-channel-links')
-						.on('mouseover', function(){
-							$('.top-ani').animate({left:300}, 3000);
-						})
-						.on('mouseout', function(){
-							$('.top-ani').animate({left:0}, 3000);
-						}); 
-*/
-				$('#top-channel-links').on('mouseover', function(){
-				
-							$('#top-ani5').animate({left:400}, 1500);
-							$('#top-ani4').delay(1000).animate({left:400}, 1500);
-							$('#top-ani3').delay(2000).animate({left:400}, 1500);
-							$('#top-ani2').delay(3000).animate({left:400}, 1500);
-							$('#top-ani1').delay(4000).animate({left:400}, 1500);
-						})
-						.on('mouseout', function(){
-							
-							$('#top-ani1').animate({left:0}, 500);
-							$('#top-ani2').delay(1000).animate({left:0}, 500);
-							$('#top-ani3').delay(2000).animate({left:0}, 500);
-							$('#top-ani4').delay(300).animate({left:0}, 500);
-							$('#top-ani5').delay(4000).animate({left:0}, 500);
-							$('#top-channel-links').clearQueue();
+		$('#top-channel-links').hover(function(){
+					$('.top-ani').animate({left:400}, 2000);
+				}, function(){
+					$('.top-ani').animate({left:0}, 3000);
 				});
-				
-				/*$('#top-channel-links').on('mouseover', function(){
-						var ani = '.top-ani';
-						
-						
-						ani.each(function(index){
-							ani.delay(index*500).animate({left:400}, 500);
-						})
-						.on('mouseout', function(){
-							$('.top-ani').each(function(index){
-							$('this').delay(index*1000).animate({left:0}, 2000);
-							})
-						})
-				});*/
-				
-	$('#myPageIcon').on('click',function hrefMove(){
-					
-					 location.href = "html/login.html";
-					
-				
-	});
-
 });
--- teamProject_Group5/src/main/webapp/js/MainPage/languageWin.js
+++ teamProject_Group5/src/main/webapp/js/MainPage/languageWin.js
@@ -7,31 +7,10 @@
 	
 	$('#kor').on('click',function(){	
 		$('#selectcon').slideToggle()
-			
+		
+		
+
+		
 	});
-	
-	$('#uk').on('click',function hrefMove(){
-					
-					 location.href = "html/uk-index.html";
-					
-				
-	});
-	
-	$('#ita').on('click',function hrefMove(){
-					
-					 location.href = "html/uk-index.html";
-					
-				
-	});
-	
-	$('#frc').on('click',function hrefMove(){
-					
-					 location.href = "html/uk-index.html";
-					
-				
-	});
-	
-	
-	
 	
 	});
