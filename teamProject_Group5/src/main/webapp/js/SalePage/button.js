50e42919fc5d6795b61a5d8ad83f98bc7f833ce0
JHooniie <cliffear324@gmail.com>
2022-06-27 22:04:01 +0900

    final

---
+++ teamProject_Group5/src/main/webapp/js/SalePage/button.js
@@ -0,0 +1,21 @@
+        //              	window.onload = function(){
+		// 		var btn = document.getElementById("btn");
+
+		// 		for(var i=0; i<btn.length; i++){
+		// 			btn[i].onclick =function() {
+		// 				alert("구매하시겠습니까?.");
+
+
+		// 		}
+		// 		}
+							
+        // };
+   
+window.onload = function(){
+var btn = document.getElementById('btn');
+btn.onclick = function(){
+alert("구매하시겠습니까?");
+
+}
+
+}
